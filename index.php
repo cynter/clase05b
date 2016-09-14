fe34a95f21168d9d6f9c204c4bc9193b0fc8786f
cynter <cynteran27@gmail.com>
2016-09-13 22:05:35 -0300

    35 incompleto

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form action="archivo_destino.php" method="post">
+	<input type="submit" name="guardar" value ="Guardar">
+
+	<?php 
+
+
+
+	 ?>
+</form>
+</body>
+</html>
